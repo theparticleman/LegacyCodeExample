8be6834289803ac340407f6fcff84f298cfd533d
Jonathan Turner <jonathan-turner@pluralsight.com>
2014-03-09 19:39:25 -0600

    Add a stub form to use in our tests to get them passing (with a REALLY big caveat).

---
+++ PictureViewer.Tests/PictureViewerPresenterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PictureViewer.Tests
+{
+    [TestFixture]
+    public class PictureViewerPresenterTests
+    {
+        [Test]
+        public void SelectedImageChangedShouldDoSomething()
+        {
+            var formStub = new PictureViewerStub
+            {
+                ImagesLocation = @"C:\Users\jonathan-turner\Pictures\Wallpapers", //This has to be a real directory on disk.
+                SelectedImage = @"137801.jpg" //This has to be the name of a real file in that directory.
+            };
+            var presenter = new PictureViewerPresenter(formStub);
+
+            presenter.SelectedImageChanged();
+        }
+    }
+
+    class PictureViewerStub : IPictureViewerForm
+    {
+        public string CurrentImageLocation { get; set; }
+        public string ImagesLocation { get; set; }
+        public string SelectedImage { get; set; }
+    }
+}
