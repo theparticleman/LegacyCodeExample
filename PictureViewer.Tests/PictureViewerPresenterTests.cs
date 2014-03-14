3fd355a7e44935c18b9ed5faeaa9e94190bf8b14
Jonathan Turner <jonathan-turner@pluralsight.com>
2014-03-14 07:15:25 -0600

    Added Moq and removed hand coded PictureViewerStub

--- PictureViewer.Tests/PictureViewerPresenterTests.cs
+++ PictureViewer.Tests/PictureViewerPresenterTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 
 namespace PictureViewer.Tests
@@ -12,15 +13,15 @@ namespace PictureViewer.Tests
     {
         private const string NewImagesLocation = @"C:\new images location";
         private FileDependenciesStub fileDependencies;
-        private PictureViewerStub formStub;
+        private Mock<IPictureViewerForm> formStub;
         private PictureViewerPresenter presenter;
 
         [SetUp]
         public void SetUp()
         {
             fileDependencies = new FileDependenciesStub();
-            formStub = new PictureViewerStub();
-            presenter = new PictureViewerPresenter(formStub, fileDependencies);
+            formStub = new Mock<IPictureViewerForm>();
+            presenter = new PictureViewerPresenter(formStub.Object, fileDependencies);
         }
 
         [Test]
@@ -31,23 +32,23 @@ namespace PictureViewer.Tests
                 @"C:\file1.jpg",
                 @"C:\file2.jpg"
             };
-            formStub.ImagesLocation = @"C:\any made up directory";
-            formStub.SelectedImage = @"file1.jpg";
+            formStub.Setup(x => x.ImagesLocation).Returns(@"C:\any made up directory");
+            formStub.Setup(x => x.SelectedImage).Returns(@"file1.jpg");
 
             presenter.SelectedImageChanged();
 
-            Assert.That(formStub.CurrentImageLocation, Is.EqualTo(@"C:\file1.jpg"));
+            formStub.VerifySet(x => x.CurrentImageLocation = @"C:\file1.jpg");
         }
 
         [Test]
         public void UpdateImagesLocationShouldSetImagesLocationToNewImagesLocation()
         {
-            formStub.ShouldUpdateImagesLocation = true;
-            formStub.NewImagesLocation = NewImagesLocation;
+            formStub.Setup(x => x.ShouldUpdateImagesLocation).Returns(true);
+            formStub.Setup(x => x.NewImagesLocation).Returns(NewImagesLocation);
 
             presenter.UpdateImagesLocation();
 
-            Assert.That(formStub.ImagesLocation, Is.EqualTo(NewImagesLocation));
+            formStub.VerifySet(x => x.ImagesLocation = NewImagesLocation);
         }
 
         [Test]
@@ -59,7 +60,7 @@ namespace PictureViewer.Tests
 
             presenter.Initialize();
 
-            Assert.That(formStub.ImagesLocation, Is.EqualTo(NewImagesLocation));
+            formStub.VerifySet(x => x.ImagesLocation = NewImagesLocation);
         }
 
         [Test]
@@ -74,34 +75,9 @@ namespace PictureViewer.Tests
 
             presenter.ImagesLocationChanged();
 
-            Assert.That(formStub.ImagesList.Contains("file1.jpg"));
+            formStub.Verify(x => x.AddImageToList("file1.jpg"));
         }
 
-    }
-
-    class PictureViewerStub : IPictureViewerForm
-    {
-        private readonly List<string> imagesList = new List<string>();
-
-        public List<string> ImagesList
-        {
-            get { return imagesList; }
-        }
-
-        public string CurrentImageLocation { get; set; }
-        public string ImagesLocation { get; set; }
-        public string SelectedImage { get; set; }
-        public bool ShouldUpdateImagesLocation { get; set; }
-        public string NewImagesLocation { get; set; }
-        public void ClearImagesList()
-        {
-            imagesList.Clear();
-        }
-
-        public void AddImageToList(string imageName)
-        {
-            imagesList.Add(imageName);
-        }
     }
 
     class FileDependenciesStub : IFileDependencies
